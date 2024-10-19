7137adc1efe27a6d3737d78168a35b1891e11514
anto <anthonybouchereau@gotmail.com>
2024-10-19 21:45:56 +0200

    f

--- examples/OutputSidWebAudioRust.mjs
+++ examples/OutputSidWebAudioRust.mjs
@@ -17,17 +17,17 @@ export default class OutputSidWebAudioRust  {
         this.scriptNode.connect(this.audioContext.destination);    */
         
         this.scriptNode.push(await new AudioWorkletNode(this.audioContext, 'SidWorklet', {processorOptions: {"sidVoice":0}}));
-        this.scriptNode.push(await new AudioWorkletNode(this.audioContext, 'SidWorklet', {processorOptions: {"sidVoice":1}}));
-        this.scriptNode.push(await new AudioWorkletNode(this.audioContext, 'SidWorklet', {processorOptions: {"sidVoice":2}}));
+       // this.scriptNode.push(await new AudioWorkletNode(this.audioContext, 'SidWorklet', {processorOptions: {"sidVoice":1}}));
+      //  this.scriptNode.push(await new AudioWorkletNode(this.audioContext, 'SidWorklet', {processorOptions: {"sidVoice":2}}));
         this.panner.push(this.audioContext.createStereoPanner());
-        this.panner.push(this.audioContext.createStereoPanner());
-        this.panner.push(this.audioContext.createStereoPanner());
+       // this.panner.push(this.audioContext.createStereoPanner());
+      //  this.panner.push(this.audioContext.createStereoPanner());
         this.gain.push(this.audioContext.createGain());
-        this.gain.push(this.audioContext.createGain());
-        this.gain.push(this.audioContext.createGain());
+     //   this.gain.push(this.audioContext.createGain());
+      //  this.gain.push(this.audioContext.createGain());
 
 
-        for(let i=0;i<3;i++) {
+        for(let i=0;i<1;i++) {
             this.scriptNode[i].connect(this.gain[i]);        
             this.gain[i].connect(this.panner[i]);     
             this.panner[i].connect(this.audioContext.destination);
